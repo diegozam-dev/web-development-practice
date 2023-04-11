5f32aa7d08c633fa0a371c455247d1070f9e7e82
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2023-04-10 21:07:06 -0500

    Responsive design

--- navbar-1/index.js
+++ navbar-1/index.js
@@ -1,16 +1,27 @@
-const nav = document.querySelector('nav');
+const navItems = document.querySelector('.nav__items');
+const navBtnOpen = document.querySelector('#nav__btn-open');
+const navBtnClose = document.querySelector('#nav__btn-close');
 
-const stickyNav = (e) => {
-    if(window.scrollY <= 80) {
-        nav.style.removeProperty('position');
-        nav.style.removeProperty('top');
-        nav.style.removeProperty('background-color');
-        return;
-    }
-
-    nav.style.position = 'sticky';
-    nav.style.top = '0';
-    nav.style.backgroundColor = '#1C3879';
+const showMenu = () => {
+    navItems.style.right = '0';
+    navBtnOpen.style.display = 'none';
+    navBtnClose.style.display = 'flex';
 }
 
-window.addEventListener('scroll', stickyNav);
+const hideMenu = () => {
+    navItems.style.right = '-100%';
+    navBtnClose.style.display = 'none';
+    navBtnOpen.style.display = 'flex';
+}
+
+const resetNavAndBtns = () => {
+    if(window.innerWidth > 768) {
+        navBtnOpen.style.removeProperty('display');
+        navBtnClose.style.removeProperty('display');
+        navItems.style.removeProperty('right');
+    }
+}
+
+window.addEventListener('resize', resetNavAndBtns);
+navBtnOpen.addEventListener('click', showMenu);
+navBtnClose.addEventListener('click', hideMenu);
