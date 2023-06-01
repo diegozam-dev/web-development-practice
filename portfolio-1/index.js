09dd41303313fb585b47610c10d6aa09517234fb
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2023-05-31 20:11:04 -0500

    refactoring contact section

--- portfolio-1/index.js
+++ portfolio-1/index.js
@@ -1,5 +1,5 @@
 const navPrincipal = document.querySelector('#nav-principal');
-const navPrincipalBtnMenu = document.querySelector('.nav-principal__btn-menu');
+const navPrincipalBtnMenu = document.querySelector('#nav-principal__btn-menu');
 const navFooter = document.querySelector('.nav-footer');
 const footer = document.querySelector('footer');
 
@@ -7,22 +7,16 @@ navPrincipalBtnMenu.addEventListener('click', () => {
     navPrincipalBtnMenu.classList.toggle('menu--open');
 });
 
-window.addEventListener('scroll', (e) => {
-
-    // console.log(navFooter.getBoundingClientRect().height)
-    
+window.addEventListener('scroll', (e) => {    
     let posNavFooter = navFooter.getBoundingClientRect().height;
     let posFooter = footer.getBoundingClientRect().height;
     
-
     if(window.scrollY > 49 && window.scrollY < document.body.clientHeight - 617 - posNavFooter - posFooter) {
         navPrincipal.style.position = 'sticky';
         navPrincipal.style.top = '0';
         navPrincipal.style.backgroundColor = 'var(--color-primary-900)';
         return;
     }
-
-
 
     navPrincipal.style.removeProperty('position');
     navPrincipal.style.removeProperty('top');
