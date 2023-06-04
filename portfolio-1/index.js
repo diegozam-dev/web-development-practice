2bfb64131a80ac9f10f26af69b0f75fa3a84802e
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2023-06-04 10:43:35 -0500

    refactoring javascript functions

--- portfolio-1/index.js
+++ portfolio-1/index.js
@@ -1,19 +1,21 @@
 const navPrincipal = document.querySelector('#nav-principal');
 const navPrincipalBtnMenu = document.querySelector('#nav-principal__btn-menu');
 const navPrincipalMenu = document.querySelector('.nav-principal__menu');
-const navFooter = document.querySelector('#nav-footer');
-const footer = document.querySelector('footer');
 
-navPrincipalBtnMenu.addEventListener('click', () => {
+const openAndCloseMenu = () => {
     navPrincipalBtnMenu.classList.toggle('btn-menu-open');
-    navPrincipalMenu.classList.toggle('menu-open')
-});
+    navPrincipalMenu.classList.toggle('menu-open');
+    document.body.classList.toggle('scroll-block');
+}
 
-window.addEventListener('scroll', (e) => {    
-    let posNavFooter = navFooter.getBoundingClientRect().height;
-    let posFooter = footer.getBoundingClientRect().height;
-    
-    if(window.scrollY > 49 && window.scrollY < document.body.clientHeight - 617 - posNavFooter - posFooter) {
+const resetNavMenu = () => {
+    navPrincipalBtnMenu.classList.remove('btn-menu-open');
+    navPrincipalMenu.classList.remove('menu-open');
+    document.body.classList.remove('scroll-block');
+}
+
+const stickNavAtTop = () => {
+    if(window.scrollY > 49) {
         navPrincipal.style.position = 'sticky';
         navPrincipal.style.top = '0';
         navPrincipal.style.backgroundColor = 'var(--color-primary-900)';
@@ -23,7 +25,16 @@ window.addEventListener('scroll', (e) => {
     navPrincipal.style.removeProperty('position');
     navPrincipal.style.removeProperty('top');
     navPrincipal.style.removeProperty('background-color');
-});
+}
 
+navPrincipalBtnMenu.addEventListener('click', openAndCloseMenu);
+
+window.addEventListener('scroll', stickNavAtTop);
+
+window.addEventListener('resize', (e) => {
+    if(document.documentElement.scrollWidth >= 767) {
+        resetNavMenu();
+    }
+})
 
 
